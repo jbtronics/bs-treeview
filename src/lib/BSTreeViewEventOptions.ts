5d231caca0f064eb10c2024cf31f16059ce0851a
Jan Böhmer <mail@jan-boehmer.de>
2022-08-08 23:51:03 +0200

    Removed unused method options

--- src/lib/BSTreeViewEventOptions.ts
+++ src/lib/BSTreeViewEventOptions.ts
@@ -4,9 +4,6 @@
 export default class BSTreeViewEventOptions {
     /** If true no events will be triggered by this action */
     silent: boolean = false;
-    ignoreChildren: boolean = false;
-
-    lazyLoad: boolean = false;
 
     /**
      * Force a change of the node state, even if the vaklue is not changed. Mostly useful for internal usage
